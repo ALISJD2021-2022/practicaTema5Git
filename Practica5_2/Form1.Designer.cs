a5f508604c8373071dd5ef16f31193ebe4b14e63
Simon Domich <domich20127@iesmarenostrum.com>
2022-02-23 19:26:03 +0100

    Segunda version, con formulario y notas

--- Practica5_2/Form1.Designer.cs
+++ Practica5_2/Form1.Designer.cs
@@ -29,21 +29,104 @@ namespace Practica5_2
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.aluNombre = new System.Windows.Forms.TextBox();
+            this.aluNota = new System.Windows.Forms.TextBox();
+            this.listaAlumnos = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre alumno";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(261, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nota";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(367, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 20);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Guardar alumno";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // aluNombre
+            // 
+            this.aluNombre.Location = new System.Drawing.Point(55, 42);
+            this.aluNombre.Name = "aluNombre";
+            this.aluNombre.Size = new System.Drawing.Size(173, 20);
+            this.aluNombre.TabIndex = 3;
+            // 
+            // aluNota
+            // 
+            this.aluNota.Location = new System.Drawing.Point(264, 42);
+            this.aluNota.Name = "aluNota";
+            this.aluNota.Size = new System.Drawing.Size(39, 20);
+            this.aluNota.TabIndex = 4;
+            // 
+            // listaAlumnos
+            // 
+            this.listaAlumnos.Location = new System.Drawing.Point(55, 105);
+            this.listaAlumnos.Multiline = true;
+            this.listaAlumnos.Name = "listaAlumnos";
+            this.listaAlumnos.ReadOnly = true;
+            this.listaAlumnos.Size = new System.Drawing.Size(434, 177);
+            this.listaAlumnos.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Lista de alumnos";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(546, 300);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listaAlumnos);
+            this.Controls.Add(this.aluNota);
+            this.Controls.Add(this.aluNombre);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ALISJD2021-2022 Practica 5.2";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox aluNombre;
+        private System.Windows.Forms.TextBox aluNota;
+        private System.Windows.Forms.TextBox listaAlumnos;
+        private System.Windows.Forms.Label label3;
     }
 }
 
